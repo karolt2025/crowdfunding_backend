1153324a0b207da241c76f932a4bb0150827e765
karoltravino <travino.karol@gmail.com>
2025-09-06 11:48:59 +0800

    added import validationerror

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.forms import ValidationError
 
 # class Fundraiser(models.Model):
 #     title = models.CharField(max_length=200)
